845065a993fb55c1c44bb5e385fa04e1111ed369
Abdalla Mohamed El-Abd <abdalla.mohamed.elabd@gmail.com>
2016-05-25 07:53:54 +0200

    Fixed routes of repositories states.

--- app/repositories/route.js
+++ app/repositories/route.js
@@ -2,7 +2,7 @@ angular.module('ngGitHub').config(function ($stateProvider) {
     $stateProvider
         .state('repositories', {
             url: '/Repositories',
-            templateUrl: '/repositories/repositories.partial.html',
+            templateUrl: 'repositories/repositories.partial.html',
             controller: 'repositoriesCtrl',
             title: 'Repositories'
         });
--- app/repository/route.js
+++ app/repository/route.js
@@ -2,7 +2,7 @@ angular.module('ngGitHub').config(function ($stateProvider) {
     $stateProvider
         .state('repository', {
             url: '/Repository/:owner/:name',
-            templateUrl: '/repository/repository.partial.html',
+            templateUrl: 'repository/repository.partial.html',
             controller: 'repositoryCtrl',
             title: 'Repository'
         });
